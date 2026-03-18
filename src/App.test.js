65cb8fa865aaaa8c07bf0cc9b8e8dda0780a9fe7
Filipp Ruskih <filippruskih@gmail.com>
2026-03-18 08:33:31 +0800

    edit tests

--- src/App.test.js
+++ src/App.test.js
@@ -16,15 +16,3 @@ test('renders portfolio header', () => {
   const header = screen.getByRole('heading', { level: 1 });
   expect(header).toBeInTheDocument();
 });
-
-test('Back button exists', () => {
-  render(<App />);
-  const backButton = screen.getByText(/back to overview/i);
-  expect(backButton).toBeInTheDocument();
-});
-
-test('shoot images render', () => {
-  render(<App />);
-  const images = screen.getAllByRole('img');
-  expect(images.length).toBeGreaterThan(0);
-});
