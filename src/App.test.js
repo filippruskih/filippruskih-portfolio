bb4769573a118327f535a0045d89ac94662a51a0
Filipp Ruskih <filippruskih@gmail.com>
2026-03-18 08:31:22 +0800

    adding in simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,21 @@ test('renders portfolio name', () => {
   const textElements = screen.getAllByText(/filipp/i);
   expect(textElements.length).toBeGreaterThan(0);
 });
+
+test('renders portfolio header', () => {
+  render(<App />);
+  const header = screen.getByRole('heading', { level: 1 });
+  expect(header).toBeInTheDocument();
+});
+
+test('Back button exists', () => {
+  render(<App />);
+  const backButton = screen.getByText(/back to overview/i);
+  expect(backButton).toBeInTheDocument();
+});
+
+test('shoot images render', () => {
+  render(<App />);
+  const images = screen.getAllByRole('img');
+  expect(images.length).toBeGreaterThan(0);
+});
